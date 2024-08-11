d3ec7609e32e872cf3ec9a4d562ee820b918dcf2
Bharath Singalareddy <bharathsingalareddy@gmail.com>
2024-08-11 14:31:35 +0530

    Created controller for todo items

---
+++ MyToDo/WrbAPI/Controllers/ToDoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : Controller
+    {
+        List<ToDoModel> _toDoList = new List<ToDoModel>();
+        public ToDoController()
+        {
+            _toDoList.Add(new ToDoModel() { Id = 1, Title="Task 1"});
+            _toDoList.Add(new ToDoModel() { Id = 2, Title="Task 2"});
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public List<ToDoModel> GetAll()
+        {
+            return _toDoList;
+        }
+    }
+}
---
+++ MyToDo/WrbAPI/Models/ToDoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Models
+{
+    public class ToDoModel
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+    }
+}
